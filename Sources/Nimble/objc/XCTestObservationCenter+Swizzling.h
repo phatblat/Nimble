e92e1b6543da5318970956404e2a7a13a66e27a3
Ben Chatelain <ben@octop.ad>
2016-03-26 19:55:21 -0600

    Add XCTestObservationCenter+Swizzling category

---
+++ Sources/Nimble/objc/XCTestObservationCenter+Swizzling.h
@@ -0,0 +1,14 @@
+//
+//  XCTestObservationCenter+Swizzling.h
+//  Test
+//  Copied from https://git.yopeso.com/iOS/Swetler/blob/a1504f450862bdb1aea27b8990618b426ce240b2/IntegrationTests/SwetlerTestsPrettyPrinting/TXCCleanReporter/XCTestObservationCenter+Swizzling.h
+//
+//  Created by Andrei Raifura on 9/9/15.
+//  Copyright © 2015 thelvis. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XCTestObservationCenter (Swizzling)
+
+@end
