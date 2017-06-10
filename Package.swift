19b73a7f6b45405b60ca24ab23cf72c44478499f
Ben Chatelain <ben@octop.ad>
2017-06-10 12:02:54 -0600

    Update package for SwiftPM 4.0

--- Package.swift
+++ Package.swift
@@ -1,12 +1,28 @@
-// swift-tools-version:3.0
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "Nimble",
-    exclude: [
-        "Sources/Lib",
-        "Sources/NimbleObjectiveC",
-        "Tests/NimbleTests/objc",
-    ]
+    products: [
+        .library(name: "Nimble", targets: ["Nimble"])
+    ],
+    targets: [
+        .target(
+            name: "Nimble",
+            dependencies: [],
+            path: "Sources/Nimble"
+        ),
+        .testTarget(
+            name: "NimbleTests",
+            dependencies: [
+                .target(name: "Nimble"),
+            ],
+            path: "Tests/NimbleTests",
+            exclude: [
+                "objc",
+            ]
+        ),
+    ],
+    swiftLanguageVersions: [4]
 )
